9fa12d7a9c5cdb00f96b7021d96dcfe063b2df52
Rod Schmidt <rod@infinitenil.com>
2017-02-04 18:54:45 -0700

    Add tests for when there are errors

--- DependinatorTests/DependinatorTests.swift
+++ DependinatorTests/DependinatorTests.swift
@@ -21,10 +21,11 @@ class DependinatorSpecs: QuickSpec {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             sut = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
             sut.webService = webServiceMock
-            XCTAssertNotNil(sut.view)  // load the view
         }
 
         describe("view setup") {
+            beforeEach { _ = sut.view }
+
             it("has an activityIndicator") {
                 expect(sut.activityIndicator) != nil //.toNot(beNil())
             }
@@ -39,28 +40,50 @@ class DependinatorSpecs: QuickSpec {
         }
 
         describe("activity indicator") {
+            beforeEach { _ = sut.view }
+            
             it("stops the activityIndicator") {
                 expect(sut.activityIndicator.isAnimating).toEventually(beFalse())
             }
         }
 
         describe("loading") {
-            beforeEach { webServiceMock.getDateResult = .success(self.makeTestDate()) }
+            context("when successful") {
+                beforeEach {
+                    webServiceMock.getDateResult = .success(self.makeTestDate())
+                    _ = sut.view
+                }
 
-            it("loads the data") {
-                expect(sut.detailItem).toEventuallyNot(beNil())
+                it("loads the data") {
+                    expect(sut.detailItem).toEventuallyNot(beNil())
+                }
+
+                it ("shows the detail label") {
+                    expect(sut.detailDescriptionLabel.isHidden).toEventuallyNot(beTrue())
+                }
+
+                it("sets the label") {
+                    expect(sut.detailDescriptionLabel.text).toEventuallyNot(beNil())
+                }
+
+                it("sets the label to the right value") {
+                    expect(sut.detailDescriptionLabel.text).toEventually(equal("2017-03-07 00:00:00 +0000"))
+                }
             }
 
-            it ("shows the detail label") {
-                expect(sut.detailDescriptionLabel.isHidden).toEventuallyNot(beTrue())
-            }
+            context("when there are errors") {
+                beforeEach {
+                    webServiceMock.getDateResult = .failure("Unable to load date")
+                    _ = sut.view
+                }
 
-            it("sets the label") {
-                expect(sut.detailDescriptionLabel.text).toEventuallyNot(beNil())
-            }
+                it("should keep the detail label hidden") {
+                    expect(sut.detailDescriptionLabel.isHidden) == true
+                }
 
-            it("sets the label to the right value") {
-                expect(sut.detailDescriptionLabel.text).toEventually(equal("2017-03-07 00:00:00 +0000"))
+                it("should stop the activity indicator") {
+                    expect(sut.activityIndicator.isAnimating) == false
+                }
             }
         }
     }
