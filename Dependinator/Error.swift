d127ade31ae2086435f801b66115699970b38c21
Rod Schmidt <rod@infinitenil.com>
2017-02-04 18:40:52 -0700

    Add ability to use String as Error

---
+++ Dependinator/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  Dependinator
+//
+//  Created by Rod Schmidt on 2/4/17.
+//  Copyright © 2017 infiniteNIL. All rights reserved.
+//
+
+import Foundation
+
+// Makes Strings errors
+
+extension String: Error {}
